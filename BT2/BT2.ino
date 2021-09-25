7972b228dff1f02a11460491634954d70ba252b9
tamnguyen662001 <82741052+tamnguyen662001@users.noreply.github.com>
2021-09-25 16:08:15 +0700

    upload BT2

---
+++ BT2/BT2.ino
@@ -0,0 +1,16 @@
+int x = 0;
+void setup()
+{
+  pinMode(2,INPUT);
+  pinMode(13,OUTPUT);
+}
+void loop()
+{
+  x = digitalRead(2);
+  if(x == HIGH){
+    digitalWrite(13,HIGH);
+  }else{
+    digitalWrite(13,LOW);
+  }
+  delay(1000);
+}
