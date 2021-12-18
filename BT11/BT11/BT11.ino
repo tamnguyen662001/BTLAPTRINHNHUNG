8e10355fc9c31256ddf3c3eb591e4ea73e8f5077
Nguyễn Lê Thành Tâm <82741052+tamnguyen662001@users.noreply.github.com>
2021-12-18 22:03:38 +0700

    chỉnh sửa bài tập 11 led ma trận

--- BT11/BT11/BT11.ino
+++ BT11/BT11/BT11.ino
@@ -1,27 +1,47 @@
-#define shcp_hang 13
-#define ds_hang 12
-
-#define shcp_cot 3
-#define ds_cot 4
-
-#define stcp 11
-
-void setup() {
-  pinMode(shcp_hang, OUTPUT);
-  
-  pinMode(ds_hang, OUTPUT);
-  
-  pinMode(shcp_cot, OUTPUT);
-  
-  pinMode(ds_cot, OUTPUT);
-  
-
+int lock_hang = 13;
+int lock_cot = 7;
+int latch = 11;
+int data_hang = 12;
+int data_cot = 6;
+byte row[8]={
+  B00111100,
+  B01100110,
+  B01100110,
+  B01111110,
+  B01100110,
+  B01100110,
+  B01100110,
+  B01100110
+};
+byte hex[8]={
+    B11111110,
+    B11111101,
+    B11111011,
+    B11110111,
+    B11101111,
+    B11011111,
+    B10111111,
+    B01111111
+};
+void setup()
+{
+  pinMode(lock_hang, OUTPUT);
+  pinMode(lock_cot, OUTPUT);
+  pinMode(latch, OUTPUT);
+  pinMode(data_hang, OUTPUT);
+  pinMode(data_cot, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(stcp,0);
-  shiftOut(ds_hang,shcp_hang,MSBFIRST,B00000111);
-  shiftOut(ds_cot,shcp_cot,MSBFIRST,B11111000);
-  digitalWrite(stcp,1);
-
+void loop()
+{
+  for(int i=0;i<8;i++){
+    digitalWrite(latch, LOW);
+    shiftOut(data_hang,lock_hang,MSBFIRST,row[i]);
+    shiftOut(data_cot,lock_cot,MSBFIRST,hex[i]);
+    digitalWrite(latch, HIGH);
+  }
+//  digitalWrite(latch, LOW);
+//    shiftOut(data_hang,lock_hang,MSBFIRST,B00111100);
+//    shiftOut(data_cot,lock_cot,MSBFIRST,B01111110);
+//    digitalWrite(latch, HIGH);
 }
